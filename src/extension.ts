be04c546552d5d8c56163a5e9cbddc332326f85a
simnJS <simon42400@gmail.com>
2022-08-18 00:15:38 +0200

    First Commit

---
+++ src/extension.ts
@@ -0,0 +1,73 @@
+import * as vscode from "vscode";
+import * as fse from "fs-extra";
+
+const path = require("path");
+export function activate(context: vscode.ExtensionContext) {
+
+/*
+Create ts bot template
+use this to create a new bot template
+*/
+	let createTsBotFile = vscode.commands.registerCommand('sheweny-utility.createtsbotfile', () => {
+
+		try {
+        const currentFolderPath =
+        vscode.workspace.workspaceFolders![0].uri["fsPath"];
+		
+
+
+        const jsTemplateFolder = path.join(
+            vscode.extensions.getExtension("simnJS.sheweny-utility")!.extensionPath,
+            "src",
+            "tsTemplate"
+          );
+
+          fse.copy(jsTemplateFolder, currentFolderPath, function (err: any) {
+            if (err) {
+              vscode.window.showErrorMessage("Failed to create the bot template!");
+            }
+            vscode.window.showInformationMessage("Created bot template!");
+          });
+		} catch (error) {
+			vscode.window.showErrorMessage("Please open a workspace first");
+		}
+	});
+/*
+Create js bot template
+use this to create a new bot template
+*/
+
+let createJsBotFile = vscode.commands.registerCommand('sheweny-utility.createjsbotfile', () => {
+
+	try {
+	const currentFolderPath =
+	vscode.workspace.workspaceFolders![0].uri["fsPath"];
+	
+
+
+	const jsTemplateFolder = path.join(
+		vscode.extensions.getExtension("simnJS.sheweny-utility")!.extensionPath,
+		"src",
+		"jsTemplate"
+	  );
+
+	  fse.copy(jsTemplateFolder, currentFolderPath, function (err: any) {
+		if (err) {
+		  vscode.window.showErrorMessage("Failed to create the bot template!");
+		}
+		vscode.window.showInformationMessage("Created bot template!");
+	  });
+	} catch (error) {
+		vscode.window.showErrorMessage("Please open a workspace first");
+	}
+});
+
+
+
+
+
+	context.subscriptions.push(createTsBotFile);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
---
+++ src/jsTemplate/src/interactions/modals/modal.ts
@@ -0,0 +1,12 @@
+import { Modal, type ShewenyClient } from "sheweny";
+import type { ModalSubmitInteraction } from "discord.js";
+
+export class ModalTest extends Modal {
+  constructor(client: ShewenyClient) {
+    super(client, ["modal-id"]);
+  }
+  
+  async execute(modal: ModalSubmitInteraction) {
+    modal.reply("Modal received.");
+  }
+};
